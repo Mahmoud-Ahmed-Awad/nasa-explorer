0433aab11bd46b77af1eb69f95766bba127adf0a
Mahmoud-Ahmed-Awad <mahmodawadahmed@gmail.com>
2025-10-05 01:14:07 +0300

    footor update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -133,7 +133,7 @@ const Footer = () => {
         {/* Bottom Bar */}
         <div className="border-t border-slate-800 mt-8 pt-8 flex flex-col md:flex-row justify-between items-center">
           <p className="text-slate-400 text-sm">
-            © 2024 NASA Explorer. All rights reserved.
+            © 2025 NASA Explorer. All rights reserved.
           </p>
           <div className="flex space-x-6 mt-4 md:mt-0">
             <a
